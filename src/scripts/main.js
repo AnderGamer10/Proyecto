413bcdd3aab70fade5469663ed2cadf75878807f
AnderGamer10 <anderedondo10@hotmail.com>
2021-12-29 20:52:58 +0100

    añadiendo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,7 +16,40 @@ crearNodos();
 //Funcion para crear los nodos 
 function crearNodos(){
     for(i = 0; i < aDatos.length;i++){
+        
         var marker = L.marker([aDatos[i].GpxY, aDatos[i].GpxX]).addTo(mapa);
         marker.bindPopup(`${aDatos[i].Nombre}`);    
+        marker.on("click", añadir);
     }
+};
+seleccionados = [];
+//Funcion para saber que marcador a clicado
+function añadir(e) {
+    var sValorNombre = e.target.getPopup().getContent();
+    /*-----------  Por hacer -----------
+    En esta funcion se obtendra los valores de la temperatura
+    */ 
+    for(i = 0; i < aDatos.length;i++){
+        if(sValorNombre == aDatos[i].Nombre){
+            id = aDatos[i].Id;
+            //Se añadira a un array para saber si esta dentro o no
+            if(seleccionados.indexOf(id) == -1 && seleccionados.length < 5){
+                seleccionados.push(id);
+                var crearDiv = "";
+                crearDiv += 
+                `
+                <div id="opcion${id}" class="opcionElegida">
+                    <p>${aDatos[i].Nombre}</p>
+                </div>
+                `;
+                document.getElementById("seleccionados").innerHTML += crearDiv;
+            }else{
+                //Mejorar la alerta--------------------------------------------------
+                alert("No se puede añadir ya que ha superado la cantidad de lugares seleccionados o ya la ha seleccionado")
+            }
+            break;
+        }
+    }
+
+    console.log(id);
 };
