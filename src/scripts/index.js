0af23b57c82fe66cc0f36dd73a7ea21454585a6c
AnderGamer10 <anderedondo10@hotmail.com>
2022-02-01 12:55:35 +0100

    fetch login completado

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,42 @@
     /* ---------------------------------- Login -------------------------------------- */
 window.enviarDatos = enviarDatos;
-function enviarDatos(){
-    window.location="main.html";
+// function enviarDatos(){
+//     window.location="main.html";
+// }
+
+function enviarDatos() {
+    fetch("https://localhost:5001/Users/authenticate", {
+        method: "POST",
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: document.getElementById("typeUsernameX").value,
+          password: document.getElementById("typePasswordX").value,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((err) => {
+          console.log(err);
+         });
 }
+
+
+// function enviarDatos(datos){
+//     $.ajax({
+//             data: datos,
+//             url: "https://localhost:5001/Users/authenticate",
+//             type: 'post',
+//             success:  function (response) {
+//                 console.log(response); // Imprimir respuesta del archivo
+//             },
+//             error: function (error) {
+//                 console.log(error); // Imprimir respuesta de error
+//             }
+//     });
+// }
     // $(document).ready(function () {
         // $("#btn-login").on("click", function () {
         //     var formData = $(form).serialize();
@@ -15,17 +49,4 @@ function enviarDatos(){
         // })
     // })
    
-    // function enviarDatos(datos){
-    //     $.ajax({
-    //             data: datos,
-    //             url: "https://localhost:5001/Users/authenticate",
-    //             type: 'post',
-    //             success:  function (response) {
-    //                 console.log(response); // Imprimir respuesta del archivo
-    //             },
-    //             error: function (error) {
-    //                 console.log(error); // Imprimir respuesta de error
-    //             }
-    //     });
-    // }
     /* ------------------------------- Fin Login ------------------------------------- */
